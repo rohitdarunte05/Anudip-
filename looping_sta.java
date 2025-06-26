1f1c3a69654266c427af627e36ad7d7062f8ef1a
Rohit Darunte <darunterohit05@gmail.com>
2025-06-26 17:22:15 +0530

    Add files via upload

---
+++ looping_sta.java
@@ -0,0 +1,22 @@
+public class looping_sta{
+    public static void main(String args[]){
+     System.out.println("For loop program");  
+    for(int i=0;i<=5;i++)
+    {
+        System.out.println("Hello");
+    }System.out.println("\n\n\n\nWhile loop");
+    int i=1;
+    while (i<=5) {
+    
+        System.out.println(i);
+        i++;
+    }
+System.out.println("\n\n\n\nDo while Program");
+int j=1;
+do{
+    System.out.println("hii");
+    j++;
+}
+while(j<=4);
+}
+}
